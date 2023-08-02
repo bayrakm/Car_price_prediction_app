d1380f4f9659975976e23e282c9dea6fe17c6d46
bayrakm <muhammetbayrak@gmail.com>
2023-08-02 14:20:17 +0100

    Readme added

---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+# Used Car Price Prediction App
+This is a simple web app built with Streamlit to predict used car prices based on key attributes like make, model, mileage, and year. It uses a pre-trained machine learning model to generate the price predictions.
+
+## Demo
+A live demo of the app is available here: 
+
+## How it Works
+The app takes in user inputs for mileage, year, make, and model. These are processed into a feature vector that is passed to a pre-trained regression model. The model then makes a single price prediction which is displayed to the user.
+
+The model itself was trained on used car listing data scraped from eBay Kleinanzeigen. The training data includes details like make, model, year, mileage, and price for used car ads. This was used to fit a Random Forest Regressor model using Scikit-Learn.
+
+The trained model is serialized with Pickle and loaded into the Streamlit app.
+
+## Running the App Locally
+To run the app locally, clone this repo and create a new conda environment:
+
+git clone https://github.com/bayrakm/Car_price_prediction_app.git
+
+conda create -n car-app-env python=3.10
+
+Install dependencies:
+requirements.txt
+
+streamlit run app.py
+
+The app will be available at http://localhost:8501. Adjust the code as needed to use your own model and data.
--- app.py
+++ app.py
@@ -9,15 +9,24 @@ xgb_model = joblib.load('xgb_4f')
 # Title
 st.title("Used Car Price Prediction")
 
+# Description
+text = '''This app predicts the market price of a used car based on key attributes like mileage, age, make and model.
+         Simply input the details of the car and hit 'Predict' to get an estimated price! 
+         The prediction is made using a machine learning model trained on car sale price data.
+         Read more about the project and model training in [this article](https://medium.com/@muhammet/building-a-dynamic-price-prediction-model-tackling-concept-drift-in-automobile-markets-a7b6c52112c2). 
+         Try out the app below and see the model in action!
+'''
+st.markdown(text)
+
 make_model = pd.read_csv('make_model.csv', index_col=0)
 
 make_ = make_model.columns.tolist()
 
 # User inputs
-mileage = st.number_input("Mileage", min_value=0, max_value=1000000, value=10000)
+mileage = st.number_input("Mileage", min_value=0, max_value=100000, value=0)
 year = st.number_input("Year", min_value=2000, max_value=2022, value=2000)
-make = st.selectbox("Make", [''] + make_)
-model = st.selectbox("Model", [''] + make_model[make].tolist())
+make = st.selectbox("Make", make_, placeholder="Toyota")
+model = st.selectbox("Model", make_model[make].tolist())
 
 # Create feature vector
 features = [year, mileage]
@@ -40,4 +49,10 @@ features = np.array([features])
 # Make prediction
 if st.button("Predict"):
     price = xgb_model.predict(features)
-    st.success(f"The predicted price is £{price[0]:.2f} with +/- £{price[0]*0.1102:.2f} price error range")
+    st.subheader(f'The predicted price is £{price[0]:.2f}')
+    text = f'''
+        The predicted price assumes the car is in average condition for {year} {make} {model}.
+        Actual price may vary between £{price[0] - price[0]*0.1102:.2f} to £{price[0] + price[0]*0.1102:.2f} 
+        depending on overall condition and any mechanical issues.
+'''
+    st.success(text)
